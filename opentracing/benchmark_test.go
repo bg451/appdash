b7b9faa479aa4947d9811c1dae6a48f2ff8d0b38
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2016-03-02 21:27:19 -0500

    opentracing: Use protobufs for binary enc/decoding
    opentracing: Add inject/join benchmarks
    
    opentracing: Stop encoding empty baggage

--- opentracing/benchmark_test.go
+++ opentracing/benchmark_test.go
@@ -70,3 +70,50 @@ func BenchmarkSpan_1000Tags(b *testing.B) {
 func BenchmarkSpan_100Attributes(b *testing.B) {
 	benchmarkWithOps(b, 0, 0, 100)
 }
+
+func benchmarBinaryCarrier(b *testing.B, numTags int, benchJoin bool) {
+	var r noopCollector
+	recorder := appdash.NewRecorder(appdash.SpanID{}, &r)
+	t := (NewTracer(recorder)).(*Tracer)
+	c := opentracing.NewSplitBinaryCarrier()
+	sp := t.StartSpan("tester")
+	for i := 0; i < numTags; i++ {
+		sp.SetBaggageItem(tags[i], tags[i])
+	}
+	t.binaryPropagator.Inject(sp, c)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if benchJoin {
+			_, _ = t.binaryPropagator.Join("suss", c)
+		} else {
+			t.binaryPropagator.Inject(sp, c)
+		}
+	}
+
+}
+
+func BenchmarkBinaryJoin_0BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 0, true)
+}
+func BenchmarkBinaryJoin_10BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 10, true)
+}
+func BenchmarkBinaryJoin_100BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 100, true)
+}
+func BenchmarkBinaryJoin_1000BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 1000, true)
+}
+
+func BenchmarkBinaryInject_0BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 0, false)
+}
+func BenchmarkBinaryInject_10BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 10, false)
+}
+func BenchmarkBinaryInject_100BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 100, false)
+}
+func BenchmarkBinaryInject_1000BaggageItems(b *testing.B) {
+	benchmarBinaryCarrier(b, 1000, false)
+}
